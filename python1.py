d3d905451060ff422c719f139fa12e700a5888a6
Yan-1-kuang <2815549503@qq.com>
2026-09-21 09:07:33 +0800

    自动输出一小时hello, world

--- python1.py
+++ python1.py
@@ -1,9 +1,5 @@
-x = float(input('x = '))
-if x > 1:
-    y = 3 * x - 5
-elif x >= -1:
-    y = x + 2
-else:
-    y = 5 * x + 3
-print(f'{y = }') #大括号里面变量名后面写个等号，自动打印「变量名 = 值」
-print(y )
+
+import time
+for i in range(3600):
+    print('hello, world')
+    time.sleep(1)
