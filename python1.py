eb107861d37c68823c6e461085e209f360fc108f
Yan-1-kuang <2815549503@qq.com>
2026-09-21 12:28:30 +0800

    列表叠加

--- python1.py
+++ python1.py
@@ -1,6 +1,9 @@
 #列表操作
 list1 = [1, 2, 3, 4, 5]
 print(list1)
-lange=['hinese','ath','english']
-lange.append('french')
+lange=['Chinese','Math','English','Chinese']
+lange.append('French')
 print(lange)
+a=list1 + lange
+print(a)
+print(list1 + lange)
