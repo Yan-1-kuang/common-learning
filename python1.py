46d31262c225a12a56064cb10d8077b0334267b8
Yan-1-kuang <2815549503@qq.com>
2026-09-21 12:54:30 +0800

    集合

--- python1.py
+++ python1.py
@@ -1,26 +1,11 @@
-s1 = 'hello, world!'
-s2 = "你好，世界！"
-s3 = '''hello,
-wonderful
-world!'''
-print(s1)
-print(s2)
-print(s3)
-s1 = '\it \is \time \to \read \now'
-s2 = r'\it \is \time \to \read \now'
-#\t、\r和\n都是转义字符。\t是制表符（table），\n是换行符（new line），
-# \r是回车符（carriage return）相当于让输出回到了行首。
-# Python 中有一种以r或R开头的字符串，这种字符串被称为原始字符串，意思是字符串中的每个字符都是它本来的含义，
-# 没有所谓的转义字符。
-print(s1)
-print(s2)
+set1 = {1, 2, 3, 3, 3, 2}
+print(set1)  # {1, 2, 3}
 
-print(ord('a')) 
+set2 = {True, False, True, True, False}
+print(set2)  # {False, True}
 
-s = 'hello, world!'
-print(s.find('or'))      # 8
-print(s.find('or', 9))   # -1
-print(s.find('of'))      # -1
-print(s.index('or'))     # 8
-print(s.index('or', 9)) 
-#find方法找不到指定的字符串会返回-1，index方法找不到指定的字符串会引发ValueError错误。
+set3 = set('hello')
+print(set3)  # {'l', 'o', 'e', 'h'}
+
+set4 = set([1, 2, 2, 3, 3, 3, 2, 1])
+print(set4)  # {1, 2, 3}
