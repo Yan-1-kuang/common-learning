f85cecf63bd438938a8bcb34dc818b846a0f014c
Yan-1-kuang <2815549503@qq.com>
2026-09-21 09:28:00 +0800

    列表操作

--- python1.py
+++ python1.py
@@ -1,7 +1,3 @@
-sum_even = 0
-for i in range(1, 101):
-    if i % 2 == 0:
-        sum_even = sum_even + i
-print(sum_even)
-while (sum_even > 0):
-    print("python启动")
+#列表操作
+list1 = [1, 2, 3, 4, 5]
+print(list1)
