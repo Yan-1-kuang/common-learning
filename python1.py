5a9c523c739034bcc69b69d0c5e2b0a87829ff3d
Yan-1-kuang <2815549503@qq.com>
2026-09-22 11:41:09 +0800

    今日练习

--- python1.py
+++ python1.py
@@ -1,22 +1,7 @@
-#字典
-d1 = {'name': '骆昊', 'age': 43, 'is_student': True}
-print(d1)
-print(len(d1))
+from math import factorial
 
-person = {'name': '王大锤', 'age': 55, 'height': 168, 'weight': 60, 'addr': '成都市武侯区科华北路62号1栋101'}
+m = int(input('m = '))
+n = int(input('n = '))
+print(factorial(m) // factorial(n) // factorial(m - n))
 
-# 成员运算
-print('name' in person)  # True
-print('tel' in person)   # False
-
-# 索引运算
-print(person['name'])
-print(person['addr'])
-person['age'] = 25
-person['height'] = 178
-person['tel'] = '13122334455'
-person['signature'] = '你的男朋友是一个盖世垃圾，他会踏着五彩祥云去迎娶你的闺蜜'
-print(person)
-
-for key in person:
-    print(f'{key}:\t{person[key]}')
+print ("你好，世界")
